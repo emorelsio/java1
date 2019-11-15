4dfd3f8b6d94ae4721d41b0029b9dd846ff75b88
emorelsio <emilienmorel09@gmail.com>
2019-11-15 15:48:45 +0100

    troisieme

--- src/org/ldv/sio/Main.java
+++ src/org/ldv/sio/Main.java
@@ -5,9 +5,13 @@ public class Main {
   public static void main(String[] args) {
     Client c = new Client("Dijsktra", "Edsger");
     Adresse a = new Adresse("Dijkstra", "3 rue du clocher", "Melun", "77000");
-    Client c2 = new Client("Kernighan", "Brian");
-    Adresse a2 = new Adresse("Kernighan","2 rue du cimetiere", "Paris", "75001");
-    System.out.println(c.toString() + a.toAdresse());
-    System.out.println(c2.toString() + a2.toAdresse());
+    adresseLivraison al = new adresseLivraison("", "", "", "");
+    System.out.println(c.toString());
+    System.out.println("Adresse de domicile: " + a.toAdresse());
+    if (al.toAdresseLiv().equals("") == false) {
+      System.out.println("Adresse de livraison: " + a.toAdresse());
+    } else {
+      System.out.println("Adresse de livraison: " + al.toAdresseLiv());
+    }
   }
 }
