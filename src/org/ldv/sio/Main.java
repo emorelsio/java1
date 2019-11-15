60fe6b75d3c01776b5f693f8dd63a1ccb601eb2a
emorelsio <emilienmorel09@gmail.com>
2019-11-15 15:07:32 +0100

    premier

--- src/org/ldv/sio/Main.java
+++ src/org/ldv/sio/Main.java
@@ -3,7 +3,11 @@ package org.ldv.sio;
 public class Main {
 
   public static void main(String[] args) {
-    Client c = new Client("nom", "prenom");
-    System.out.println(c.toString());
+    Client c = new Client("Dijsktra", "Edsger");
+    Adresse a = new Adresse("Dijkstra", "3 rue du clocher", "Melun", "77000");
+    Client c2 = new Client("Kernighan", "Brian");
+    Adresse a2 = new Adresse("Kernighan","2 rue du cimetiere", "Paris", "75001");
+    System.out.println(c.toString() + a.toAdresse());
+    System.out.println(c2.toString() + a2.toAdresse());
   }
 }
