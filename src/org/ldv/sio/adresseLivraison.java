c2d19691b86b5a1499a836a52190308a462558d0
emorelsio <emilienmorel09@gmail.com>
2019-11-18 10:27:07 +0100

    quatrieme

--- src/org/ldv/sio/adresseLivraison.java
+++ src/org/ldv/sio/adresseLivraison.java
@@ -1,4 +1,57 @@
 package org.ldv.sio;
 
 public class adresseLivraison {
+
+    private String nom;
+    private String rue;
+    private String ville;
+    private String codePostal;
+
+    public adresseLivraison(String nom, String rue, String ville, String codePostal) {
+        this.nom = nom;
+        this.rue = rue;
+        this.ville = ville;
+        this.codePostal = codePostal;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String toAdresseLiv() {
+        return "Adresse : {" +
+                "nom='" + nom + '\'' +
+                ", rue='" + rue + '\'' +
+                ", code postal='" + codePostal + '\'' +
+                ", ville='" + ville + '\'' +
+                '}';
+    }
 }
